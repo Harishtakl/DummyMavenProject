93c32afd8cc6a623d9be764952fb6df7c988e7e0
htakle <htakle@1800packrat.com>
2021-04-23 15:45:39 +0530

    test one

--- Javaproject/src/Baseclass.java
+++ Javaproject/src/Baseclass.java
@@ -1,15 +1,7 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import com.aventstack.extentreports.ExtentTest;
 
 public class Baseclass {
 	
